a60bb0d1ee54111b163e568e69c0da9ef68684ee
Kay Herrmann <kay.herrmann@ict-campus.net>
2024-05-30 16:15:16 +0200

    added delete

---
+++ src/functions/deleteItems.js
@@ -0,0 +1,20 @@
+const { app } = require('@azure/functions');
+const { CosmosClient } = require('@azure/cosmos');
+
+const client = new CosmosClient(process.env.CosmosDB);
+const container = client.database('DemoDatabase').container('Items');
+
+app.http('deleteItem', {
+    methods: ['DELETE'],
+    authLevel: 'anonymous',
+    route: 'items/delete',
+    handler: async (request, context) => {
+        const item = await request.json();
+        const { id } = item;
+        await container.item(id).delete();
+        return {
+            body: JSON.stringify({ message: 'Item deleted successfully' }),
+            status: 200
+        };
+    }
+});
