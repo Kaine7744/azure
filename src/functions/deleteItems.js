772727ba731645774a9b4a9995e61618d86f62dd
Kay Herrmann <kay.herrmann@ict-campus.net>
2024-05-31 09:53:35 +0200

    delete tried: tried to add delete, failed miserably

--- src/functions/deleteItems.js
+++ src/functions/deleteItems.js
@@ -3,7 +3,6 @@ const { CosmosClient } = require('@azure/cosmos');
 
 const client = new CosmosClient(process.env.CosmosDB);
 const container = client.database('DemoDatabase').container('Items');
-const partitionKey = "UfcOVvPTuh3TXBuILh4Um7C9WBHmtr1oT13EueqmWKdy3Rn5XqYTUEsS0xKQ5k3NBfAkXZ8FN73yACDbZEwS9g==";
 
 app.http('deleteItem', {
     methods: ['DELETE'],
